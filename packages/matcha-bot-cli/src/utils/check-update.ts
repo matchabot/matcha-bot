747fe7b551ff1f5bed92fcc24fcce19448706d19
Raphael MANSUY <raphael.mansuy@caliatys.com>
2021-01-29 09:10:40 +0800

    improve update notifier

--- packages/matcha-bot-cli/src/utils/check-update.ts
+++ packages/matcha-bot-cli/src/utils/check-update.ts
@@ -3,11 +3,16 @@ import Package from "../../package.json"
 
 export const checkUpdate = () => {
   // Checks for available update and returns an instance
-  const notifier = updateNotifier({ pkg: Package })
+  const notifier = updateNotifier({
+    pkg: Package,
+    updateCheckInterval: 1000 * 60 * 30 // 30 minutes
+  })
 
   // Notify using the built-in convenience method
   notifier.notify()
 
   // `notifier.update` contains some useful info about the update
-  console.log(notifier.update)
+  if (notifier.update) {
+    console.log(`Update available: ${notifier.update.latest}`)
+  }
 }
