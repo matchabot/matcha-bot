4355f6e42652dd1b0264914e1e6923db0c96cfd3
Raphael MANSUY <raphael.mansuy@caliatys.com>
2021-02-08 21:50:56 +0800

    update documenntation

--- packages/matcha-bot-doc/gatsby-config.js
+++ packages/matcha-bot-doc/gatsby-config.js
@@ -5,9 +5,13 @@ module.exports = {
       options: {
         name: "MatchBot Documentation",
         title: "MatchaBot Documentation",
-        description: "Use your own description...",
-        siteUrl: "https://example.com",
-      },
-    },
-  ],
-};
+        author: "Raphaël MANSUY",
+        navItems: [{ title: 'Docs', url: '/docs/' }],
+        githubRepositoryURL: "https://github.com/matchabot/matcha-bot",
+        description:
+          "A powerful project code generator, simple and fun to use. Improve your daily productivity for your React, Gatsby and NextJS projects.",
+        siteUrl: "https://example.com"
+      }
+    }
+  ]
+}
