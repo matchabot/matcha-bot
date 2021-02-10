e129ded58200054137765c65ccfee5cb1cfbe87a
Raphael MANSUY <raphael.mansuy@caliatys.com>
2021-02-10 09:39:07 +0800

    update documenation and fix sitemap

--- packages/matcha-bot-doc/gatsby-config.js
+++ packages/matcha-bot-doc/gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require("path")
+
 module.exports = {
   plugins: [
     {
@@ -12,19 +14,25 @@ module.exports = {
           "A powerful project code generator, simple and fun to use. Improve your daily productivity for your React, Gatsby and NextJS projects.",
         siteUrl: "https://www.matchabot.site",
         baseDirectory: path.resolve(__dirname, "../"),
-        sections: ["Introduction"],
-        twitterAccount: "raphaelmansuy",
-      },
+        sections: ["Introduction","Credits"],
+        twitterAccount: "raphaelmansuy"
+      }
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-189279883-1",
-      },
+        trackingId: "UA-189279883-1"
+      }
     },
     {
       resolve: "gatsby-plugin-sitemap",
-    },
-  ],
-};
+      options: {
+        resolveSiteUrl: ({ site, allSitePage }) => {
+          //Alternatively, you may also pass in an environment variable (or any location) at the beginning of your `gatsby-config.js`.
+          return "https://www.matchabot.site"
+        }
+      }
+    }
+  ]
+}
